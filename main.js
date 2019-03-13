e50e242aa4dad1b6d62c600507aaf3dc3b920086
iinourah96@gmail.com <iinourah96@gmail.com>
2019-03-14 00:18:35 +0300

    add a comment

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     let player1 = "X";
     let player2 = "O";
-    // $(".winner").text(player1 + "'s turn")
+    // score variable  
     let player1Score =0 ;
     let player2Score =0 ;
-    
+    // i declare the current Turn variable to switch between player 
     let currentTurn=1;
     let moves = 0 ;
     
@@ -16,96 +16,105 @@ $(document).ready(function(){
             event.target.innerHTML= player1;
             event.target.style.color ="black";
             currentTurn++;
-    $(".winner").text(player2 + "'s Turn")
-            
-            }
+            $(".winner").text(player2 + "'s Turn")
+
+        }
             else
             {   
             event.target.innerHTML= player2;
             event.target.style.color= "white";
             currentTurn--;
         $(".winner").text(player1 + "'s Turn")
+    }
 
-            }
-                if (checkWinner()){  
-            let theWinner = currentTurn==1?player2:player1;
-                
+// check The winner 
+function checkWinner(){
+    if (moves > 4){
+    // call a prototype method on array and the slice array to return a subset of our array so it's gonna a new array on the start and end of the index we use ..
+    let x = Array.prototype.slice.call($(".box")) ;
+    //here we gonna go through the whole array and return just the innerHtml property 
+        var result = x.map(function(box){
+        return box.innerText;
+    
+    });
+    console.log(result)
+    
+    // i create arrays to all possible ways to win in XO 
+    var winChance=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+    ];
+    
+        return winChance.find(function(chance){
+            if (result[chance[0]]!=="" && result[chance[1]]!=="" && result[chance[2]]!=="" && result[chance[0]] === result[chance[1]] && result[chance[1]] === result[chance[2]]){
+                return true ;
+            } else {
+                return false ;
+    }
+    
+    })
+    
+    
+    }};
+
+        // if checkWinner function return true 
+    if (checkWinner()){ 
+            let theWinner ;
+            if( currentTurn==1)
+            console.log(theWinner = player2)
+            else (theWinner=player1)
+    // call the declare winner function 
             declareTheWinner(theWinner);
-            score(theWinner)
+            score(theWinner);
 
         }
     });
     
+
+    function declareTheWinner(winner){
+        $(".winner").css('display','block');
+            $(".winner").text(` Congratulation  ${winner}`);
+            $(".winner").addClass('zoomInUp'); // animation 
+
+
+            $(".refresh").css("display","block");
     
+        }
+        // score counter function 
+        function score(winner){
+        if (winner===player1){
+    
+            player1Score++;
+            $("#player1").html(player1Score)
+        }
+        else {
+    
+            player2Score++;
+            $("#player2").html(player2Score)
+        }
+    }
+    // clicking on play agin button 
     $(".refresh").on('click',function()
     {
-    let x = Array.prototype.slice.call($(".box")) ;
+     x = Array.prototype.slice.call($(".box")) ;
+     // remove all marks in the board 
     x.map((m) =>{
     m.innerHTML ="";
     })
     $(".winner").html("");
-    // $(".winner").css('display','none');
     currentTurn=1;
     moves =0;
 
     })
 
 
-// check The winner 
-function checkWinner(){
-if (moves > 4){
-
- x = Array.prototype.slice.call($(".box")) ;
- var result = x.map(function(box){
-    return box.innerText;
-    //  console.log(something)
-// return something
-});
-console.log(result)
 
 
-var winChance=[
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6],
-];
-
-    return winChance.find(function(chance){
-        // debugger;
-        if (result[chance[0]]!=="" && result[chance[1]]!=="" && result[chance[2]]!=="" && result[chance[0]] === result[chance[1]] && result[chance[1]] === result[chance[2]]){
-            return true ;
-        } else {
-            return false ;
-}
-
-})
-
-
-}};
-function declareTheWinner(winner){
-    $(".refresh").css("display","block");
-    $(".winner").css('display','block');
-    
-        $(".winner").text(` Congratulation  ${winner}`);
-        $(".winner").addClass('zoomInUp');
-
-    }
-    function score(winner){
-    if (winner===player1){
-
-        player1Score++;
-        $("#player1").html(player1Score)
-    }
-    else {
-
-        player2Score++;
-        $("#player2").html(player2Score)
-    }
-}
         
 });
